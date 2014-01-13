fe15d83eb64da8ecff4bfd61950efb9046292ce4
Paul Fenwick <pjf@perltraining.com.au>
2014-01-14 02:47:43 +1100

    Updated for nicer module interface

---
+++ lib/Markdown/phpBB.pm
@@ -0,0 +1,29 @@
+package Markdown::phpBB;
+
+use 5.010;
+use strict;
+use warnings;
+use autodie;
+use Moose;
+
+# ABSTRACT: Turn markdown into phpBB code
+# VERSION
+
+use Markdent::Parser;
+use Markdown::phpBB::Handler;
+
+my $handler = Markdown::phpBB::Handler->new;
+
+my $parser = Markdent::Parser->new(
+    handler => $handler,
+);
+
+sub convert {
+    my ($self, $text) = @_;
+
+    $parser->parse(markdown => $text);
+
+    return $handler->result;
+}
+
+1;
